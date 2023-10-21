17ed80edf27d55ab871287d4c77ca885bc0bbf9d
Artem Fedotovskikh <artemfedotovskih69@gmail.com>
2023-10-21 15:08:41 +0500

    Fix slider

--- main/index.php
+++ main/index.php
@@ -1,4 +1,4 @@
-<?php require './Header.php'; ?>
+<?php require '../Header.php'; ?>
 <body>
     <main>
         <div>
@@ -13,29 +13,29 @@
                 <h1 class="title">Калькулятор строительства</h1>
                 <div class="maincalc">
                     <div class='home_container'>
-                        <img src='#' alt='Homemini' class='home'></img>
+                        <img src='../assets/homemini.png' alt='Homemini' class='home'></img>
                     </div>
                     <div class='choice'>
                         <form class='formcalc'>
-                            <center>
-                                <Slider />
+                            <h2 class='S'>Площадь дома,<span id="demo"></span>м	&#178;</h2>
+                            <div class="slidecontainer">
+                                <input type="range" min="1" max="1000" value="50" class="slider" id="myRange"></div>
                                 <select class='select'>
                                     <option class='option' disabled>Количество этажей</option>
                                     <option class='option'>1</option>
                                     <option class='option'>2</option>
                                 </select>
                                 <button class='continue'>Далее</button>
-                            </center>
                         </form>
                     </div>
                 </div>
             </div>
-            <?php require './aboutUs/aboutUs.php' ?>
+            <?php require '../aboutUs/aboutUs.php' ?>
         </div>
     </main>
 
- 
-    <script src="./aboutUs/aboutUs.js"></script>
+    <script src="slider.js"></script>
+    <script src="../aboutUs/aboutUs.js"></script>
 </body>
 
 </html>
