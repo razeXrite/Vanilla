9626744d3964ec8fcd3a6fac0df8fb3ea557bab5
Artem Fedotovskikh <artemfedotovskih69@gmail.com>
2023-11-02 18:31:03 +0500

    Fixed JS

--- main/slider.js
+++ main/slider.js
@@ -2,11 +2,13 @@ var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
 slider.oninput = function () {
-    output.innerHTML = this.value;
+    let dlina=output.innerHTML = this.value;
+    console.log(dlina);
 }
 var slidertwo = document.getElementById("myRangetwo");
 var outputtwo = document.getElementById("demotwo");
 outputtwo.innerHTML = slidertwo.value;
 slidertwo.oninput = function () {
-    outputtwo.innerHTML = this.value;
+    let shir=outputtwo.innerHTML = this.value;
+    console.log(shir);
 }
