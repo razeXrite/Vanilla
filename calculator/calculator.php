effe18fe0bee60d06577c213934f2fe53367bb75
nafana123 <makartop08@mail.ru>
2023-11-03 16:45:13 +0500

    const

--- calculator/calculator.php
+++ calculator/calculator.php
@@ -93,18 +93,32 @@ require './../Header/Head.php';
                 <img src="img/fire.png" />
                 <h3>Негорючесть</h3>
               </div>
-
+            
               <div class="material">
+                <div class="contName">
                 <h2>Материалы:</h2>
                 <ul>
-                  <li>Фундамент теплового контура <span id="fundCost">0</span> руб.</li>
-                  <li>Фундамент крыльца <span>12 938</span> руб.</li>
-                  <li>Фундамент террасы <span>7 763</span> руб.</li>
+                  <li>Фундамент теплового контура</li>
+                  <li>Фундамент крыльца</li>
+                  <li>Фундамент террасы</li>
                 </ul>
               </div>
 
+              <div class="contConst">
+                <li id="fundCost"></li>
+                <li>12 938</li>
+                <li>7 763</li> 
+              </div>  
+              
+              <div class="Const">
+                <li>руб.</li>
+                <li>руб.</li>
+                <li>руб.</li> 
+              </div>
+        </div>
+
               <div class="price">
-                <h3>200 000руб.</h3>
+                <h3 >200 000руб.</h3>
                 <button class="modal_btn">Добавить</button>
               </div>
             </div>
